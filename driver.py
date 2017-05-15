13b1c7715877283a0e565a62fb9fe658429d6463
Francesco De Pace <depazz.91@gmail.com>
2017-05-15 17:50:09 +0200

    Prova male Queue

--- driver.py
+++ driver.py
@@ -5,7 +5,19 @@ Created on Mon May 15 12:46:56 2017
 @author: FraDepa
 """
 
+import Queue
+
 def hello():
     print("Hello World")
 
 hello()
+
+q = Queue.Queue()
+
+for i in range(7):
+    q.put(i)
+    
+while not q.empty():
+    print(q.get())
+    
+    
